ae427cdbb698bbd227333f1f39e71d31161eaf6d
ibalnst <ibaldoci@gmail.com>
2021-10-06 04:27:00 +0700

    ECOMM V2

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://notebookspec.com/web/pc/pic-pc/vga/20200902-104425_1.png',
+    title: 'NVIDIA 3090 TI',
+    desc: 'ONLY 5 STOCKS AVAILABLE GET 30% BUNDLING WITH TOWER CASE',
+    bg: '#fa5',
+  },
+  {
+    id: 2,
+    img: 'https://www.imagensempng.com.br/wp-content/uploads/2020/11/0018-3.png',
+    title: 'NVIDIA 3080 TI',
+    desc: 'ONLY 7 STOCKS AVAILABLE GET 15% BUNDLING WITH AAA GAME',
+    bg: '#ddf1e9',
+  },
+  {
+    id: 3,
+    img: 'https://www.pny.com/productimages/4AC2CBA6-935C-4A7A-B26C-AC2B008A4DC3/images/8-PNY-GeForce-RTX-3070-DF-M-gr.png',
+    title: 'NVIDIA 3070 TI',
+    desc: 'ONLY 17 STOCKS AVAILABLE GET 10% BUNDLING WITH STEELSERIES CASE',
+    bg: '#DE8971',
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://www.gigabyte.com/FileUpload/global/news/1635/o201808201633495922.png',
+    title: 'NVIDIA 2080 TI',
+  },
+  {
+    id: 2,
+    img: 'https://asset.msi.com/resize/image/global/product/product_160516119064accdd6d4e26f33a723921155fac476.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png',
+    title: 'RYZEN 6800 XT',
+  },
+  {
+    id: 3,
+    img: 'https://dlcdnwebimgs.asus.com/gain/9f964af3-c3c1-4b49-8dc9-3a5814c43bc1/',
+    title: 'NVIDIA 3080 TI',
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: 'https://onestop.net.bd/wp-content/uploads/2019/12/AMD-Ryzen-5-3400G.jpg',
+  },
+  {
+    id: 2,
+    img: 'https://i.ebayimg.com/images/g/70kAAOSwYUxgkRRZ/s-l300.jpg',
+  },
+  {
+    id: 3,
+    img: 'https://www.skytech.lt/images/large/97/2829397.jpg',
+  },
+  {
+    id: 4,
+    img: 'https://cdn.wccftech.com/wp-content/uploads/2021/01/Intel-11th-Gen-desktop-Rocket-Lake-S-1-1536x1075.jpg',
+  },
+  {
+    id: 5,
+    img: 'https://hardzone.es/app/uploads-hardzone.es/2020/10/Intel-Core-11-Gen.jpg',
+  },
+  {
+    id: 6,
+    img: 'https://cdn.mos.cms.futurecdn.net/Rca4E2TNdR8StnPio73Qpe-480-80.jpg',
+  },
+  {
+    id: 7,
+    img: 'https://www.tejar.pk/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/a/m/amd_ryzen_7_5800x_processor1_-_tehjar.jpg',
+  },
+  {
+    id: 8,
+    img: 'https://cdn.wccftech.com/wp-content/uploads/2020/04/AMD-Ryzen-3-CPU.jpg',
+  },
+];
