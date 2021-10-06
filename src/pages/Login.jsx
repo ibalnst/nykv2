a4eb0014e705b89b725bb994845df56ffef6f102
CLIFFrs F <cliffrsf@CLIFFrss-MacBook-Pro.local>
2021-10-06 20:06:30 +0700

    Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,72 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+width: 100vw;
+height: 100vh;
+background:
+linear-gradient(rgba(255,255,255,0.3),rgba(255,255,255,0.3)),
+url(https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/wallpapers/rtx-3090/3090-wallpaper-3440x1440-r1.png);
+background-position: center;
+background-size:cover;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  padding:20px;
+  width: 40%;
+  background-color: white;
+`;
+const Title = styled.h1`
+font-size:24px;
+font-weight: 300;
+`;
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+`;
+const Input = styled.input`
+flex:1;
+min-width:25%;
+margin: 10px 0px ;
+padding: 10px;
+`;
+// const Agreement = styled.span`
+// font-size: 12px;
+// margin:20px 0px
+// `;
+const Button = styled.button`
+width: 40%;
+border: none;
+padding:15px 20px;
+background-color: teal;
+color: white;
+cursor:pointer;
+margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+margin:5px 0px;
+font-size: 12px;
+text-decoration:underline;
+cursor: pointer;
+`;
 
 const Login = () => {
-  return <Container></Container>;
+  return <Container>
+  <Wrapper>
+    <Title>Log In</Title>
+    <Form>
+      <Input placeholder="email / username"/>
+      <Input placeholder="password"/>
+      <Button>Sign In</Button>
+      <Link>Forgot your password?</Link>
+      <Link>Don't have? Create Now!</Link>
+    </Form>
+  </Wrapper>
+</Container>;
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,7 +8,7 @@ background:
 linear-gradient(rgba(255,255,255,0.3),rgba(255,255,255,0.3)),
 url(https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/wallpapers/rtx-3090/3090-wallpaper-3440x1440-r1.png);
 background-position: center;
-background-size:contain;
+background-size:cover;
 display: flex;
 align-items: center;
 justify-content: center;
@@ -19,11 +19,32 @@ const Wrapper = styled.div`
   width: 40%;
   background-color: white;
 `;
-const Title = styled.h1``;
-const Form = styled.form``;
-const Input = styled.input``;
-const Agreement = styled.div``;
-const Button = styled.button``;
+const Title = styled.h1`
+font-size:24px;
+font-weight: 300;
+`;
+const Form = styled.form`
+display: flex;
+flex-wrap: wrap;
+`;
+const Input = styled.input`
+flex:1;
+min-width:40%;
+margin: 20px 10px 0px 0px ;
+padding: 10px;
+`;
+const Agreement = styled.span`
+font-size: 12px;
+margin:20px 0px
+`;
+const Button = styled.button`
+width: 40%;
+border: none;
+padding:15px 20px;
+background-color: teal;
+color: white;
+cursor:pointer;
+`;
 
 const Register = () => {
   return <Container>
@@ -37,7 +58,7 @@ const Register = () => {
         <Input placeholder="password"/>
         <Input placeholder="confirm password"/>
         <Agreement>
-          By Crating an account, I consent ot the processing of my personal data in accordance with <b>PRIVACY POLICY</b>
+          By Creating this account, I consent ot the processing of my personal data in accordance with <b>PRIVACY POLICY</b>
         </Agreement>
         <Button>Create Account</Button>
       </Form>
